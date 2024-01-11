8c1397f397611552de0f155e7e85cfc9473b8e33
Fizzbow <55476631+Fizzbow@users.noreply.github.com>
2024-01-11 13:41:01 +0800

    chore(path):add relative path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import UnoCSS from "unocss/vite";
+import path from "path";
+
+const pathSrc = path.resolve(__dirname, "src");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), UnoCSS()],
+  resolve: {
+    alias: {
+      "@": pathSrc,
+    },
+  },
 });
